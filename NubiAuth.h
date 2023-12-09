68a16840cda17bdc7045f4e69589f14053e38e00
Nubiza <104552412+Nubiza@users.noreply.github.com>
2023-12-09 18:50:52 +0700

    Update NubiAuth.h

--- NubiAuth.h
+++ NubiAuth.h
@@ -1,89 +1,14 @@
 #pragma once
-#include "HTTPRequest.hpp"
-
+#include <curl/curl.h> // use libcurl
 // See Example.cpp for example
 
+
 class NubiAuth {
-  public:
-  bool isAuth = false;
-
+private:
   std::string ApiKey = ""; // your Api Key (to get your Api Key login to auth.nubizaserver.my.id
-  std::string device_id = ""; // device id (example in NubiAuth.cpp)
+  const char* Auth_Link = "https://auth.nubizaserver.my.id/auth.php";
 
-  // login using user name only
-  // (Username Only in dashboard)
-  bool login(std::string username) {
-    if (device_id.empty() || device_id == "") {
-      printf("device_id is not set, exiting...");
-      exit(1);
-    }
-    http::Request request {
-      "http://auth.nubizaserver.my.id/auth.php"
-    };
-    const std::string body = "ApiKey="+ApiKey+"&username="+username+"&device_id=" + device_id;
-    const auto response = request.send(
-      "POST",
-      body,
-      {
-        {
-          "Content-Type", "application/x-www-form-urlencoded"
-        }
-      }
-    );
-    std::string resp = std::string({
-      response.body.begin(), response.body.end()
-    });
-    if (resp.find("User|Access") != -1) {
-      if (resp.find("device_id saved") != -1)
-      printf("Device id saved!\n");
-      isAuth = true;
-      return true;
-    }
-    else if (resp.find("User|Wrong") != -1) {
-      printf("Wrong username.\n");
-      isAuth = false;
-      return false;
-    }
-    else if (resp.find("User|Denied") != -1) {
-      printf("Device id not Authorized.\n");
-      isAuth = false;
-      return false;
-    }
-    else if (resp.find("ApiKey not found") != -1) {
-      printf("%s\n", resp.c_str());
-      isAuth = false;
-      return false;
-    }
-    else {
-      printf("Server is offline/error (?)\n");
-      isAuth = false;
-      return false;
-    }
-  }
-
-  // login using user name and password
-  // (different database, Username & Password in dashboard)
-  bool login(std::string username, std::string user_password) {
-    if (device_id.empty() || device_id == "") {
-      printf("device_id is not set, exiting...");
-      exit(1);
-    }
-    http::Request request {
-      "http://auth.nubizaserver.my.id/auth.php"
-    };
-    const std::string body = "ApiKey=" + ApiKey + "&username=" + username + "&password=" + user_password + "&device_id=" + device_id;
-    const auto response = request.send(
-      "POST",
-      body,
-      {
-        {
-          "Content-Type", "application/x-www-form-urlencoded"
-        }
-      }
-    );
-    std::string resp = std::string({
-      response.body.begin(), response.body.end()
-    });
+  bool ProcessRequest(std::string request_response) {
     if (resp.find("User|Access") != -1) {
       if (resp.find("device_id saved") != -1)
       printf("Device id saved!\n");
@@ -110,5 +35,63 @@ class NubiAuth {
       isAuth = false;
       return false;
     }
+  }
+public:
+  // Public Variables
+  bool isAuth = false;
+  std::string device_id = ""; // device id (example in NubiAuth.cpp)
+
+  static size_t WCallback(void *contents, size_t size, size_t nmemb, void *userp) //Write Callback
+  {
+      ((std::string *)userp)->append((char *)contents, size * nmemb);
+      return size * nmemb;
+  }
+
+  std::string RequestPost(char* url, char* request_headers, char* post_fields, bool verify_host = false, bool verify_peer = false) {
+    CURL *curl;
+    std::string readBuffer;
+    struct curl_slist *headers = NULL;
+    curl = curl_easy_init();
+    if (curl)
+    {
+        headers = curl_slist_append(headers, request_headers);
+        curl_easy_setopt(curl, CURLOPT_URL, url());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, 5000);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, verify_host);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, verify_peer);
+        curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        return readBuffer;
+    }
+    return "Failed";
+  }
+
+
+  // login using user name only
+  // (Username Only in dashboard)
+  bool login(std::string username) {
+    if (device_id.empty() || device_id == "") {
+      printf("device_id is not set, exiting...");
+      exit(1);
+    }
+    const std::string body = "ApiKey="+ApiKey+"&username="+username+"&device_id=" + device_id;
+    std::string resp = RequestPost(Auth_Link, "Content-Type: application/x-www-form-urlencoded", body.c_str(), true, true);
+    return ProcessRequest(resp);
+  }
+
+  // login using user name and password
+  // (different database, Username & Password in dashboard)
+  bool login(std::string username, std::string user_password) {
+    if (device_id.empty() || device_id == "") {
+      printf("device_id is not set, exiting...");
+      exit(1);
+    }
+    const std::string body = "ApiKey=" + ApiKey + "&username=" + username + "&password=" + user_password + "&device_id=" + device_id;
+    std::string resp = RequestPost(Auth_Link, "Content-Type: application/x-www-form-urlencoded", body.c_str(), true, true);
+    return ProcessRequest(resp);
   }
 };
